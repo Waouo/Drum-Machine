829441de8bf003e1d2e472a6ec30dcfdb8e677c5
waouo <f5864565@gmail.com>
2020-09-22 11:55:44 +0800

    selectorSW

--- src/jsx/All.jsx
+++ src/jsx/All.jsx
@@ -64,37 +64,80 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: '',
-            sliderValue:'',
+            sliderValue: '',
+            powerValue: '',
+            bankValue: '',
         }
         this.newQuote = this.newQuote.bind(this)
-        this.getInitialSlider = this.getInitialSlider.bind(this)
+        this.getInitialState = this.getInitialState.bind(this)
         this.onChangeSlider = this.onChangeSlider.bind(this)
+        this.onClickSelector = this.onClickSelector.bind(this)
     }
 
     newQuote() {
         store.dispatch(getActionId())
     }
 
+    onClickSelector(e) {
+        let sw
+        if (e.target.parentElement.id === 'powerSW' || e.target.id === 'powerSW') {
+            sw = {
+                powerValue: !this.state.powerValue
+            }
+        } else if (e.target.parentElement.id === 'bankSW' | e.target.id === 'bankSW') {
+            sw = {
+                bankValue: !this.state.bankValue
+            }
+        }
+            
+        this.setState(sw)
+    }
+
+
     onChangeSlider(e) {
         this.setState({
-            sliderValue: e.target.value
+            sliderValue: e.target.value,
         })
     }
 
-    getInitialSlider() {
-        this.setState({sliderValue: '50'})
+
+    getInitialState() {
+        this.setState({
+            sliderValue: '50',
+            powerValue: true,
+            bankValue: true,
+        })
     }
 
     componentDidMount() {
-        this.getInitialSlider()
+        this.getInitialState()
     }
 
 
+
+
     render() {
+
+        let powerZeroStyle={},
+            powerOneStyle,
+            bankZeroStyle,
+            bankOneStyle;
+
+        if (this.state.powerValue === true) {
+            powerOneStyle = { backgroundColor: 'blue' }
+        } else {
+            powerZeroStyle = { backgroundColor: 'blue' }
+        }
+
+        if (this.state.bankValue === true) {
+            bankOneStyle = { backgroundColor: 'blue' }
+        } else {
+            bankZeroStyle = { backgroundColor: 'blue' }
+        }
+
         return (
-            <main id='drum-machine' className='d-flex justify-content-around'>
-                <h1><i className='fas fa-mug-hot'></i><em>WAO</em></h1>
+            <main id='drum-machine' className='d-flex align-items-center justify-content-around'>
+                <h1 className='align-self-start'><i className='fas fa-mug-hot '></i><em>WAO</em></h1>
                 <section id='pads' className='d-flex flex-wrap align-items-center justify-content-around'>
                     <button className="drum-pad">Q</button>
                     <button className="drum-pad">E</button>
@@ -106,31 +149,31 @@ class App extends React.Component {
                     <button className="drum-pad">X</button>
                     <button className="drum-pad">C</button>
                 </section>
-                <section id='right-panel' className=''>
-                    <div className='d-flex flex-wrap align-items-center justify-content-center m-0' style={{width: '100%', height: '100%'}}>
-                        <div id="powerSW" className=''>
+                <section id='right-panel'>
+                    <div className='d-flex flex-wrap align-items-center justify-content-center m-0' style={{ width: '100%', height: '100%' }}>
+                        <div id="powerSWDiv">
                             <h2 id='power-title' className='text-center'>Power</h2>
-                            <div className="selector mx-auto">
-                                <div className="inner-0"></div>
-                                <div className="inner-1"></div>
+                            <div id="powerSW" className="selector d-flex mx-auto" onClick={this.onClickSelector}>
+                                <div className="inner-0" style={powerZeroStyle}></div>
+                                <div className="inner-1" style={powerOneStyle}></div>
                             </div>
                         </div>
                         <div id="display"></div>
-                        <div className="slidecontainer d-flex justify-content-center ">
+                        <div className="slidecontainer d-flex align-items-center" onClick={this.onClickSelector}>
                             <input type="range" min="1" max="100" value={this.state.sliderValue} className="slider" id="myRange"
-                                onChange={this.onChangeSlider}/>
+                                onChange={this.onChangeSlider} />
                         </div>
-                        <div id='bankSW'>
+                        <div id='bankSWDiv'>
                             <h2 id='bank-title' className='text-center'>Bank</h2>
-                            <div className="selector mx-auto">
-                                <div className="inner-0"></div>
-                                <div className="inner-1"></div>
+                            <div id='bankSW' className="selector d-flex mx-auto" onClick={this.onClickSelector}>
+                                <div className="inner-0" style={bankZeroStyle}></div>
+                                <div className="inner-1" style={bankOneStyle}></div>
                             </div>
                         </div>
                     </div>
-                    
+
                 </section>
-            </main>
+            </main >
         )
     }
 };
