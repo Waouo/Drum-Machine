4a0ae0ec0f52c6bbe1346f64e8c219aeb6a19ce0
waouo <f5864565@gmail.com>
2020-09-22 15:55:43 +0800

    test

--- src/jsx/All.jsx
+++ src/jsx/All.jsx
@@ -4,61 +4,6 @@ import { Provider, connect } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { bankOne, bankTwo } from './base.js'
 
-
-/**Redux */
-// function getActionId() {
-//     let randomNum = Math.floor(Math.random() * Math.floor(quotes.length))
-//     return {
-//         type: String(randomNum)
-//     }
-// }
-
-// const authorReducer = (state = '', action) => {
-//     switch (action.type) {
-//         case '0':
-//             return quotes[0].author
-//         case '1':
-//             return quotes[1].author
-//         case '2':
-//             return quotes[2].author
-//         case '3':
-//             return quotes[3].author
-//         case '4':
-//             return quotes[4].author
-//         case '5':
-//             return quotes[5].author
-//         default:
-//             return state
-//     }
-// }
-// const textReducer = (state = '', action) => {
-//     switch (action.type) {
-//         case '0':
-//             return quotes[0].text
-//         case '1':
-//             return quotes[1].text
-//         case '2':
-//             return quotes[2].text
-//         case '3':
-//             return quotes[3].text
-//         case '4':
-//             return quotes[4].text
-//         case '5':
-//             return quotes[5].text
-//         default:
-//             return quotes[0].text
-//     }
-// }
-
-// const reducer = combineReducers({
-//     authors: authorReducer,
-//     texts: textReducer,
-// })
-
-// const store = createStore(reducer)
-
-
-
 /**React */
 class App extends React.Component {
     constructor(props) {
